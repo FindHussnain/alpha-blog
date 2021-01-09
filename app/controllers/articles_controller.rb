009ac8625087401df6558f03eddac59cdae0e1db
Hussnain Younas <hussnainyounas143@gmail.com>
2021-01-09 22:45:41 +0500

    added form template for new article and a basic create method functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,12 @@ class ArticlesController < ApplicationController
 	def index
 		@articles = Article.all
 	end
+	def new
+		@article = Article.new
+	end
+	def create
+		@article = Article.new(params.require(:article).permit(:title, :description))
+		@article.save
+		redirect_to articles_path(@article)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 	root 'pages#home'
 	get 'about', to: 'pages#about'
-	resources 'articles', only: [:show, :index]
+	resources 'articles', only: [:show, :index, :new, :create]
+	# get 'articles', to: 'articles#index'
+	# get 'articles/:id', to: 'articles#show'
+	# get 'articles/new', to: 'articles#new'
+
 end
