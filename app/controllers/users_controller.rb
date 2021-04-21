e91e0b88bcb903e294167e138d6244b9452659b3
Hussnain Younas <hussnainyounas143@gmail.com>
2021-04-21 15:53:20 +0500

    Add user's show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,10 @@ class UsersController < ApplicationController
 			render 'new'
 		end
 	end
+	def show
+		@user = User.find(params[:id])
+		@articles = @user.articles
+	end
 	private
 		def user_params
 			params.require(:user).permit(:username, :email, :password)
