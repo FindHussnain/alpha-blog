236412e1252bd4bd5558a38bc54556ad91fb7864
Hussnain Younas <hussnainyounas143@gmail.com>
2021-04-24 15:50:59 +0500

    Add Login form for users, create/destroy user sessions, authentication and also add controller level, model level ristrictions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 		before_action :set_article, only: [:show, :edit, :update, :destroy]
+		before_action :require_user, except: [:show, :index]
+		before_action :require_same_user, only: [:edit, :update, :destroy]
 	def show
 		
 	end
@@ -14,10 +16,10 @@ class ArticlesController < ApplicationController
 	end
 	def create
 		@article = Article.new(article_params)
-		@article.user = User.first
+		@article.user = current_user
 		if @article.save
 			flash[:notice] = "Article was successfully saved!"
-			redirect_to articles_path(@article)
+			redirect_to user_path(@article.user)
 		else
 			# redirect_to new_article_path
 			render 'new'
@@ -45,5 +47,10 @@ class ArticlesController < ApplicationController
 	def article_params
 		params.require(:article).permit(:title, :description)
 	end
-
+	def require_same_user
+		if current_user != @article.user && !current_user.admin?
+			flash[:alert] = "You can only edit or delete your own article"
+			redirect_to @article
+		end
+	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:edit, :update, :show, :destroy]
+	before_action :require_user, only: [:edit, :update, :destroy]
+	before_action :require_admin_user, only: [:destroy]
+	before_action :require_same_user, only: [:edit, :update]
 	def new
 		@user = User.new
 	end
@@ -6,16 +10,15 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			flash[:notice] = "welcom #{@user.username} to the Alpha Blog, you have successfully signed up"
+			session[:user_id] = @user.id
 			redirect_to articles_path
 		else
 			render 'new'
 		end
 	end
 	def edit
-		@user = User.find(params[:id])
 	end
 	def update
-		@user = User.find(params[:id])
 		if @user.update(user_params)
 			flash[:notice] = "Your account information was successfully updated"
 			redirect_to @user
@@ -24,14 +27,34 @@ class UsersController < ApplicationController
 		end
 	end
 	def show
-		@user = User.find(params[:id])
 		@articles = @user.articles.paginate(page: params[:page], per_page: 4)
 	end
 	def index
 		@users = User.paginate(page: params[:page], per_page: 5)
 	end
+	def destroy
+		@user.destroy
+		session[:user_id] = nil if @user == current_user
+		flash[:notice] = "Account and all associated articles successfully deleted"
+		redirect_to articles_path
+	end
 	private
 		def user_params
 			params.require(:user).permit(:username, :email, :password)
+		end
+		def set_user
+			@user = User.find(params[:id])
+		end
+		def require_admin_user
+			if !current_user.admin? && @user != current_user
+				flash[:alert] = "You can only delete your own account"
+				redirect_to @user
+			end
+		end
+		def require_same_user
+			if current_user != @user
+				flash[:alert] = "You can only edit or delete your own account"
+				redirect_to @user
+			end
 		end
 end
